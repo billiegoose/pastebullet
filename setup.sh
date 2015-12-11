93f74aefc5de08d5d33cd6929211889bbd812a17
William Hilton <wmhilton@gmail.com>
2015-12-11 18:10:00 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd $(dirname "$BASH_SOURCE")
+mkdir -p config
+TMP=$(mktemp -d)
+function finish {
+  rm -rf "$TMP"
+}
+trap finish EXIT
+# Get the user identity
+curl --header "Access-Token: $PBTOKEN" \
+     https://api.pushbullet.com/v2/users/me \
+| jshon \
+| tee config/user.json
+# Get system variables
+NICKNAME="$(hostname)"
+MODEL="$(lsb_release -a 2>/dev/null | grep Description | sed 's/Description:\s*//')"
+# Format create device payload
+cat template/device.json \
+| jshon -s "$NICKNAME" -i nickname \
+        -s "$MODEL" -i model \
+| tee "$TMP/device.json"
+# Create new device
+curl --header "Access-Token: $PBTOKEN" \
+     --header 'Content-Type: application/json' \
+     --data-binary @"$TMP/device.json" \
+     --request POST \
+     https://api.pushbullet.com/v2/devices \
+| jshon \
+| tee config/device.json
